b560981ff5487170954ddb1ccf553cc4f9d52f35
nemi <nemelida@bk.ru>
2023-02-08 23:59:34 +0300

    if and ToLower

---
+++ exemple005_if/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username=Console.ReadLine();
+if (username.ToLower()=="вика")
+{
+    Console.Write("Ура это же Вика");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
