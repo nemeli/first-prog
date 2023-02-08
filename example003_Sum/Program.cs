60881d1d090e488d76f32a543d764222a9c2a637
nemi <nemelida@bk.ru>
2023-02-08 23:39:02 +0300

    sum random A and B

--- example003_Sum/Program.cs
+++ example003_Sum/Program.cs
@@ -1,3 +1,5 @@
-﻿int namberA=3;
-int namberB=5;
+﻿int namberA=new Random().Next(1,10);// 123...9
+Console.WriteLine(namberA);
+int namberB=new Random().Next(1,10);
+Console.WriteLine(namberB);
 Console.WriteLine(namberA+namberB);
